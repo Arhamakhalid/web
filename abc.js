5557a91d4bb520f0a340df6130ee5dbca4992c11
Arhamakhalid <arhamaiyani75@gmail.com>
2022-10-30 13:18:29 +0500

    completed

---
+++ abc.js
@@ -0,0 +1,107 @@
+alert("hellow")
+window.alert("HEY PEOPLES")
+
+var abc = "hellow js";
+alert(abc);
+
+var b = 10;
+alert(b);
+ 
+var a = 20;
+var b = 10;
+var c = a + b;
+
+alert(c)
+
+var a = 20;
+var b = 10;
+var c = a - b;
+
+alert(c)
+
+var a = 20;
+var b = 10;
+var c = a * b;
+
+alert(c)
+
+var a = 20;
+var b = 10;
+var c = a / b;
+
+alert(c)
+
+var a = 10;
+var b = 20;
+var c = ++a;
+
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = ++b;
+
+console.log(c);
+
+var a = 10;
+var b = 24;
+var c = a++;
+
+console.log(c);
+
+var a = 10;
+var b = 20;
+var c = b++;
+
+console.log(c);
+
+var a = 20;
+var b = 10;
+var c = --a;
+
+console.log(c)
+
+
+var a = 20;
+var b = 10;
+var c = a--;
+
+console.log(c)
+
+var a = 20;
+var b = 10;
+var c = --b;
+
+console.log(c)
+
+var a = 20;
+var b = 10;
+var c = b--;
+
+console.log(c)
+
+var abc = 10;
+var xyz = 20;
+var c = 1;
+var d = 1 + 10 * 20;
+
+console.log(d)
+
+var abc = 10;
+var xyz = 20;
+var c = 1;
+var d = (1 - 10) + 20;
+
+console.log(d)
+
+var a = "10";
+var b = "20";
+var c = a + b;
+
+alert(c)
+
+var a = 10;
+var b = 20;
+var c = a++ - --b + a - b + b--a - ++a + b;
+
+console.log(c)
